b97634fe1ef593c6531aa59803e3352184dbcb3f
timeo <timeo.baffreauleroux@ynov.com>
2025-11-14 10:23:52 +0100

    :lipstick: : Ajouter les couleur vert au backgound

--- main.go
+++ main.go
@@ -10,8 +10,12 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "route: %q", r.URL.Path)
 }
 
+func ColorHandler(w http.ResponseWriter, r *http.Request) {  
+	fmt.Fprintf(w, "<h1>Colors</h1><style>*{background-color: #006400;}</style>")  
+}
+
 func main() {
 	http.HandleFunc("/", IndexHandler)
-	// to add : color functionality
+	http.HandleFunc("/color", ColorHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
